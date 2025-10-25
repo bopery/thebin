bf741ce56a7891247e23f606fdb127283eab3ac0
bopery <lnxytech63@gmail.com>
2025-10-25 17:07:32 +0300

    Simplify app.run() by removing port setting
    Removed port configuration from app.run()

--- backend.py
+++ backend.py
@@ -1614,5 +1614,4 @@ def admin_logout():
 if __name__ == '__main__':
     create_templates()
     init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run()
